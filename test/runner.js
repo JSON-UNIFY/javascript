29409b1a9a967843048af785ff624cd7e4395790
Juan Cruz Viotti <jv@jviotti.com>
2023-03-16 20:29:35 -0400

    Add first validation case
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,26 @@
 import test from 'node:test';
 import { strict as assert } from 'node:assert';
+import { dirname, resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { validate } from '../lib/unify.js';
 
-test('xxx', async () => {
-  assert.ok(true);
-});
+const DIRNAME = dirname(fileURLToPath(import.meta.url));
+
+for (const testCase of JSON.parse(readFileSync(resolve(DIRNAME, 'validate.json'), 'utf8'))) {
+  test(`validate: ${testCase.title}`, async () => {
+    const result = await validate(testCase.instance);
+    if (testCase.valid) {
+      assert.deepEqual(result, {
+        keyword: 'https://json-schema.org/evaluation/validate',
+        absoluteKeywordLocation: 'https://json-unify.github.io/vocab-dataset/v1.json#',
+        instanceLocation: '#',
+        valid: true,
+        errors: []
+      });
+    } else {
+      assert.ok(!result.valid);
+      assert.ok(result.errors.length > 0);
+    }
+  });
+}
