2e9d7acd33560766d586d655cba1559b2834f0b9
Juan Cruz Viotti <jv@jviotti.com>
2023-03-17 20:54:09 -0400

    Implement basic "inspect()" support (#5)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/runner.js
+++ test/runner.js
@@ -3,7 +3,7 @@ import { strict as assert } from 'node:assert';
 import { dirname, resolve } from 'node:path';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'url';
-import { validate, read } from '../lib/unify.js';
+import { validate, read, inspect } from '../lib/unify.js';
 
 const DIRNAME = dirname(fileURLToPath(import.meta.url));
 
@@ -39,9 +39,32 @@ for (const testCase of importJSON(resolve(DIRNAME, 'read.json'))) {
         assert.deepEqual(result, testCase.data);
         return resolve();
       });
-      emitter.on('data', (data, output) => {
+      emitter.on('data', (data, output, annotations) => {
+        assert.ok(!annotations);
         if (output.valid) {
           result.push(data);
+        }
+      });
+    });
+  });
+}
+
+for (const testCase of importJSON(resolve(DIRNAME, 'inspect.json'))) {
+  assert.ok(testCase.instance.$id);
+  test(`inspect: ${testCase.instance.$id}`, async () => {
+    return new Promise((resolve, reject) => {
+      const result = { data: [], annotations: [] };
+      const emitter = inspect(testCase.instance);
+      emitter.on('error', reject);
+      emitter.on('end', () => {
+        assert.deepEqual(result.data, testCase.data);
+        assert.deepEqual(result.annotations, testCase.annotations);
+        return resolve();
+      });
+      emitter.on('data', (data, output, annotations) => {
+        if (output.valid) {
+          result.data.push(data);
+          result.annotations.push(annotations);
         }
       });
     });
